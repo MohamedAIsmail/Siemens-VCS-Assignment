ccd76da4c1270211ec09506ff8277b0d6493a4af
MohamedIsmail <mohamedaismail214@gmail.com>
2023-10-27 14:30:07 +0200

    Averaging of the vector

--- main.cpp
+++ main.cpp
@@ -14,11 +14,22 @@ int getSum(vector<int> givenVector){
     return sum;
 }
 
+int getAverage(vector<int> givenVector){
+    int sum = 0;
+
+    for(int index = 0; index < givenVector.size(); index++){
+        sum += givenVector[index];
+    }
+
+    return sum / givenVector.size();
+}
+
 int main(){
 
     vector<int> Vector = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     cout << "Summation of vector = " << getSum(Vector) << endl;
-
+    cout << "Average of vector = " << getAverage(Vector) << endl;
+    
     return 0;
 }
