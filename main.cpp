a4d6a2553906efce9f2e4ab981a814e435a86892
MohamedIsmail <mohamedaismail214@gmail.com>
2023-10-27 15:28:33 +0200

    Min of vector function added

--- main.cpp
+++ main.cpp
@@ -14,11 +14,24 @@ int getSum(vector<int> givenVector){
     return sum;
 }
 
+int getMin(vector<int> givenVector){
+    int min = givenVector[0];
+
+    for(int index = 0; index < givenVector.size(); index++){
+        if(givenVector[index] < min){
+            min = givenVector[index];
+        }
+    }
+
+    return min;
+}
+
 int main(){
 
     vector<int> Vector = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     cout << "Summation of vector = " << getSum(Vector) << endl;
+    cout << "Minimum of vector = " << getMin(Vector) << endl;
 
     return 0;
 }
